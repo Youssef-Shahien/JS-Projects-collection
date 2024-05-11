25803524fd34ed5f9ef04f619d053d842ac8a858
Youssef Shahien <youssefshahien156@gmail.com>
2024-05-11 12:16:11 +0300

    weather proj

--- js/main.js
+++ js/main.js
@@ -138,3 +138,152 @@ function updateTime() {
 }
 
 // End Of Stop Watch App
+// Start Of Calculator App
+const calcDisplay = document.getElementById("calc-display");
+let result;
+function appendToDisplay(input) {
+  calcDisplay.value += input;
+}
+
+function calculate() {
+  try {
+    calcDisplay.value = eval(calcDisplay.value);
+  } catch (error) {
+    calcDisplay.value = "Error";
+  }
+}
+
+function clearDisplay() {
+  calcDisplay.value = "";
+}
+// End Of Calculator App
+// Start Of RockPaperScissors App
+const choice = ["rock", "paper", "scissors"];
+
+const playDisplay = document.getElementById("playDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playChoice) {
+  const computerChoice = choice[Math.floor(Math.random() * 3)];
+  let result = "";
+  if (computerChoice === playChoice) {
+    result = "IT'S A TIE!";
+  } else {
+    switch (playChoice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "YOU WIN" : "YOU LOSE!";
+        break;
+      case "paper":
+        result = computerChoice === "rock" ? "YOU WIN" : "YOU LOSE!";
+        break;
+      case "scissors":
+        result = computerChoice === "paper" ? "YOU WIN" : "YOU LOSE!";
+        break;
+    }
+  }
+  playDisplay.textContent = `PLAYER : ${playChoice}`;
+  computerDisplay.textContent = `Computer : ${computerChoice}`;
+  if (result === "YOU LOSE!") {
+    resultDisplay.textContent = result;
+    resultDisplay.style.color = "red";
+  } else {
+    resultDisplay.textContent = result;
+    resultDisplay.style.color = "#198754";
+  }
+}
+// End Of RockPaperScissors App
+// Start Weather App
+
+const weatherApp = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "ed09755d56d34ef62d643e4cb4668509";
+
+weatherApp.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const city = cityInput.value;
+
+  if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      displayError(error);
+    }
+  } else {
+    displayError("Please Enter City");
+  }
+});
+
+async function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    throw new Error("Cloud Not Fetch Weather Data");
+  }
+  return await response.json();
+}
+
+function displayWeatherInfo(data) {
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ description, id }],
+  } = data;
+  card.textContent = "";
+  card.style.display = "flex";
+  const cityDisplay = document.createElement("h1");
+  const tempDisplay = document.createElement("p");
+  const humidityDisplay = document.createElement("p");
+  const descDisplay = document.createElement("p");
+  const weatherEmoji = document.createElement("p");
+
+  cityDisplay.textContent = city;
+  tempDisplay.textContent = `${((temp - 273.15) * (9 / 5) + 32).toFixed(1)}°F`;
+  humidityDisplay.textContent = `Humidity: ${humidity}%`;
+  descDisplay.textContent = description;
+  weatherEmoji.textContent = getWeatherEmoji(id);
+  //
+  cityDisplay.classList.add("cityDisplay");
+  tempDisplay.classList.add("tempDisplay");
+  humidityDisplay.classList.add("humidityDisplay");
+  descDisplay.classList.add("descDisplay");
+  weatherEmoji.classList.add("weatherEmoji");
+  card.appendChild(cityDisplay);
+  card.appendChild(tempDisplay);
+  card.appendChild(humidityDisplay);
+  card.appendChild(descDisplay);
+  card.appendChild(weatherEmoji);
+}
+
+function getWeatherEmoji(weatherId) {
+  switch (true) {
+    case weatherId >= 200 && weatherId < 300:
+      return "⛈️";
+    case weatherId >= 300 && weatherId < 400:
+      return "";
+    case weatherId >= 500 && weatherId < 600:
+      return "🌧️";
+    case weatherId >= 600 && weatherId < 700:
+      return "❄️";
+    case weatherId >= 700 && weatherId < 800:
+      return "🌬️";
+    case weatherId === 800:
+      return "☀️";
+    case weatherId >= 801 && weatherId < 810:
+      return "🌨️";
+    default:
+      return "❓";
+  }
+}
+function displayError(message) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add("errorDisplay");
+
+  card.textContent = "";
+  card.style.display = "flex";
+  card.appendChild(errorDisplay);
+}
+// End Weather App
